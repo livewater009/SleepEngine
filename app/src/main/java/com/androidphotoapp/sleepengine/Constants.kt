148f64d91b303deacc5c0598f2b1074055bfda19
livewizard93 <147813832+livewater009@users.noreply.github.com>
2025-09-25 07:03:32 -0700

    Updated

--- app/src/main/java/com/androidphotoapp/sleepengine/Constants.kt
+++ app/src/main/java/com/androidphotoapp/sleepengine/Constants.kt
@@ -8,8 +8,9 @@ object SleepConstants {
 //  val MIN_SLEEP_DURATION_MILLIS = TimeUnit.MINUTES.toMillis(45)
 //  val WORK_INTERVAL = 15
 
-  const val SLEEP_START_HOUR = 18
-  const val SLEEP_START_MINUTE = 0
+  val SLEEP_START_HOUR = 18      // 6 PM
+  val SLEEP_END_HOUR = 7         // 7 AM
+
   val MIN_SLEEP_DURATION_MILLIS = TimeUnit.MINUTES.toMillis(30)
   const val WORK_INTERVAL = 10
 }
--- app/src/main/java/com/androidphotoapp/sleepengine/SleepUtils.kt
+++ app/src/main/java/com/androidphotoapp/sleepengine/SleepUtils.kt
@@ -13,15 +13,26 @@ object SleepUtils {
     val minute = calendar.get(Calendar.MINUTE)
 
     val lockTimeMinutes = hour * 60 + minute
-    val sleepStartMinutes = SleepConstants.SLEEP_START_HOUR * 60 + SleepConstants.SLEEP_START_MINUTE
+    val sleepStartMinutes = SleepConstants.SLEEP_START_HOUR * 60
+    val sleepEndMinutes = SleepConstants.SLEEP_END_HOUR * 60
 
-    if (lockTimeMinutes >= sleepStartMinutes && durationMillis >= SleepConstants.MIN_SLEEP_DURATION_MILLIS) {
+    val isOvernight = sleepEndMinutes < sleepStartMinutes
+
+    val inSleepWindow = if (isOvernight) {
+      // Sleep window crosses midnight: e.g., 18:00 - 07:00
+      lockTimeMinutes >= sleepStartMinutes || lockTimeMinutes <= sleepEndMinutes
+    } else {
+      // Normal case: sleep window does not cross midnight
+      lockTimeMinutes in sleepStartMinutes..sleepEndMinutes
+    }
+
+    if (inSleepWindow && durationMillis >= SleepConstants.MIN_SLEEP_DURATION_MILLIS) {
       val startTime = lockTimeMillis + SleepConstants.MIN_SLEEP_DURATION_MILLIS
       val endTime = System.currentTimeMillis()
       val sleepScore = 0
 
       SleepLogStore.saveLog(context, SleepLog(startTime, endTime, sleepScore))
-      Log.d("SleepUtils", "Sleep log saved: Start=$startTime, End=$endTime, Score=$sleepScore")
+      Log.e("SleepUtils", "Sleep log saved: Start=$startTime, End=$endTime, Score=$sleepScore")
     }
   }
 }
